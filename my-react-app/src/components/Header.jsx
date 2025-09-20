5277aec0d3ccab8c6dd1f57caa298aedb85a47d0
Rogelio Acosta <rogeracosta2001@gmail.com>
2025-09-20 11:42:58 -0700

    Input and expense forms with remainder

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -2,40 +2,71 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
 const Header = () => {
+  const [incomeInput, setIncomeInput] = useState("");
   const [income, setIncome] = useState("");
+
+  const [expenseInput, setExpenseInput] = useState("");
   const [expense, setExpense] = useState("");
+
   const [remainder, setRemainder] = useState("");
 
+  useEffect(() => {
+    setRemainder(income - expense);
+  }, [income, expense]);
+
   const handleIncomeChange = (e) => {
-    setIncome(e.target.value);
+    setIncomeInput(e.target.value);
   };
 
   const handleIncomeSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted income:", income);
+    setIncome(incomeInput);
+
+    console.log("Submitted income:", incomeInput);
   };
 
-  async function handleExpense() {}
+  const handleExpenseChange = (e) => {
+    setExpenseInput(e.target.value);
+  };
 
-  async function handleRemainder() {}
+  const handleExpenseSubmit = (e) => {
+    e.preventDefault();
+    setExpense(expenseInput);
+
+    console.log("Submitted expense:", expenseInput);
+  };
+
 
   return (
     <div>
       <div className="main-zone">
         <div className="title">Allo.</div>
-        <div className="sub-title">
-          <form onSubmit={handleIncomeSubmit}>
+        <div className="sub-title input-boxes">
+          <form className="input-form" onSubmit={handleIncomeSubmit}>
             <label>
               Income:
               <input
                 type="text"
-                value={income} 
+                value={incomeInput}
                 onChange={handleIncomeChange}
               />
             </label>
             <button type="submit">Submit</button>
           </form>
+          <form className="input-form" onSubmit={handleExpenseSubmit}>
+            <label>
+              Expense:
+              <input
+                type="text"
+                value={expenseInput}
+                onChange={handleExpenseChange}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
           <div>{income}</div>
+          <div>{expense}</div>
+          <div>{remainder}</div>
         </div>
       </div>
     </div>
